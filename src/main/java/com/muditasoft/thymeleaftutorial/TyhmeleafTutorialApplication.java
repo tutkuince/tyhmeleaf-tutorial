aeced064dd93dfd8c7b469c1928424a4c1fb273e
tutkuince <tutku.ince@outlook.com>
2018-11-26 18:59:57 +0300

    initial commit

---
+++ src/main/java/com/muditasoft/thymeleaftutorial/TyhmeleafTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.muditasoft.thymeleaftutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyhmeleafTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyhmeleafTutorialApplication.class, args);
+	}
+}
---
+++ src/main/java/com/muditasoft/thymeleaftutorial/controller/MainController.java
@@ -0,0 +1,46 @@
+package com.muditasoft.thymeleaftutorial.controller;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public ModelAndView homePage() {
+		Map<String, Object> models = new HashMap<>();
+		models.put("username", "Tutku Ince");
+		models.put("id", 111);
+		
+		return new ModelAndView("homepage", "model", models);
+	}
+
+	@RequestMapping("/profile")
+	public String viewProfile(Model model) {
+		Map<String, Object> models = new HashMap<>();
+		models.put("title", "Mr");
+		models.put("firstName", "Tutku");
+		models.put("lastName", "Ince");
+		models.put("dateOfBirth", new GregorianCalendar(1989, 1, 15));
+		models.put("description", "a <strong>fantastic</strong> Java programmer");
+
+		model.addAttribute("model", models);
+		
+		List<String> languages = new ArrayList<>();
+		languages.add("English");
+		languages.add("Russian");
+		languages.add("Turkish");
+		
+		models.put("languages", languages);
+
+		return "profile";
+	}
+}
