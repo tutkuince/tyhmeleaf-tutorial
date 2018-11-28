aa191ac86e53504882ca1453a96a901edd730354
tutkuince <tutku.ince@outlook.com>
2018-11-28 14:11:27 +0300

    injected ResourceBundleMessageSource

--- src/main/java/com/muditasoft/thymeleaftutorial/TyhmeleafTutorialApplication.java
+++ src/main/java/com/muditasoft/thymeleaftutorial/TyhmeleafTutorialApplication.java
@@ -2,11 +2,21 @@ package com.muditasoft.thymeleaftutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class TyhmeleafTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TyhmeleafTutorialApplication.class, args);
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		source.setCacheSeconds(3600); // Refresh cache once per hour.
+		return source;
 	}
 }
